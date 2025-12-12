4eb1293913dbb0936311d621a505c25953be4ca2
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2025-12-12 13:39:55 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,12 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: new URL(DATABASE_URL).hostname,
+    family: 4,  //
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
